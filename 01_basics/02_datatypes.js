c34630d318d0d2d8dd7fb6c8e897244a5a4091dd
DINO <mzuhayerlabeeb@gmail.com>
2026-05-25 14:58:33 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  // treat all JS code as newer version
+
+// alert (3 + 3) // we are using node js not browser
+
+// number => 2 to power 53
+// bigint for large numbers
+// string => ""
+// boolean =? true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+console.log(typeof undefined);
+console.log(typeof null);
+

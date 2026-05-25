00ff7c69ff453f6c719b09168d545ae490294843
DINO <mzuhayerlabeeb@gmail.com>
2026-05-25 15:13:43 +0000

    datatype conversion in js

---
+++ 01_basics/dtconversion.js
@@ -0,0 +1,27 @@
+// let score = "45acb"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "45"=45
+// "33abc" = NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true and 0 => false
+// "" => false
+// "zuhayer" => true
+
+let someNumber = 44
+
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);

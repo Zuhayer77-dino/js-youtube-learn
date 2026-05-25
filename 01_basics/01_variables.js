27e85173121b9e6bb20744859df2bf671b9e47c3
DINO <mzuhayerlabeeb@gmail.com>
2026-05-25 14:35:38 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "mahdsd@gmail.com"
+var accountPassword = "2223"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 not allowed
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+

3a8455eb1bf897f65bf5bba8b1d3b38812b28a5a
DINO <mzuhayerlabeeb@gmail.com>
2026-05-26 04:02:22 +0000

    stack and heap concept

---
+++ 01_basics/stackheap.js
@@ -0,0 +1,20 @@
+let myname = "zuhayerlabeeb"
+
+let anothername = "labeeb"
+anothername = "zuzu"
+
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+    email: "mzuhayerlabb@gmail.com"
+    // upi: "sjshs@ibl"
+}
+let userTwo = userOne
+
+userTwo.email = "msknss@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+

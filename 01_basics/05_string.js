9d931b584f727dd47b8e254de47e5a8625f2711a
DINO <mzuhayerlabeeb@gmail.com>
2026-05-26 04:19:14 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "zuhayer"
+const repocount = 15
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const game = new String('zuhayer')
+
+console.log(game[3]);
+console.log(game.toUpperCase());
+console.log(game.charAt(1));
+console.log(game.indexOf('u'));
+
+const newString = game.substring(0, 4)
+console.log(newString);
+
+const url = "https://zuhayer.com/zuhayer%45hello"
+console.log(url.replace('%45', '-'));
+console.log(url.includes('hell'));
+
+
